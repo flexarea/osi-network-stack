14c251b4eef969bb8963b214a640a1a6c588d085
esdras <esdrasntuyenabo40@gmail.com>
2025-02-21 18:21:00 +0000

    need to fix some stuff

--- hexdump.c
+++ hexdump.c
@@ -8,7 +8,7 @@
 #include <string.h>
 #include <signal.h>
 
-int sig_val = 0;
+volatile sig_atomic_t sig_val = 0;
 
 int main(int argc, char *argv[]){
 	struct stat file_stat;	
@@ -24,46 +24,44 @@ int main(int argc, char *argv[]){
 	if (argc == 1){
 		int max = 16;
 		stdin_buffer = malloc(sizeof(max));
-		int bytes;
-		
-		if(signal(SIGINT, handle_sigint) == SIG_ERR){
+
+		if(signal(SIGINT, handler) == SIG_ERR){
 			perror("Signal");
 			free(stdin_buffer);
-			free(hex_data);
 			return 1;
 		}
 
 		// process running here
 		while(1){
-			bytes = read(0, stdin_buffer, 16);
 			if (sig_val == 1){
-				kill(getpid(), SIGTERM);
+				free(stdin_buffer);
+				return 0;
 			}
-			sleep(1);
-
+			ssize_t bytes = read(0, stdin_buffer, max);
 			if(bytes > 0){
-				if(bytes = 16){
-					hex_data = binary_to_hex(read_buffer, bytes); //convert binary to hex
-					if((bytes_written = write(1, hex_data, strlen(hex_data))) == -1){ //write to stdout
-						perror("write");
-						free(stdin_buffer);
-						free(hex_data);
-						return 1;
-					}
-					//free buffers to continuer reading
-					free(hex_data);
-					free(stdin_buffer);
-					if(hex_data == NULL){
-						perror("binary_to_hex");
-						return 1;
-					}
+				hex_data = binary_to_hex(stdin_buffer, bytes); //convert binary to hex
+				if(hex_data == NULL){
+					perror("binary_to_hex");
+					return 1;
 				}
-			free(hex_data);
+				//write data to stdout
+				if((bytes_written = write(1, hex_data, strlen(hex_data))) == -1){ 
+					perror("write");
+					free(stdin_buffer);
+					free(hex_data);
+					return 1;
+				}
+				//free buffers to continuer reading
+				free(hex_data);
+			} else if (bytes == 0){
+				break;
 			}else{
-				printf("Error reading  input\n");
+				perror("read");
+				free(stdin_buffer);
 				return 1;
 			}
 		}
+		free(stdin_buffer);
 	}else{
 		//read file
 		if((fd = open(argv[1], O_RDONLY)) == -1){
@@ -111,5 +109,7 @@ int main(int argc, char *argv[]){
 }
 
 void handler(int sig){
-	sig_val = 1;
+	if(sig == SIGINT){
+		sig_val = 1;
+	}
 }
