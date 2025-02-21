1414534a1efe94890048d12db7d8f08574754d22
esdras <esdrasntuyenabo40@gmail.com>
2025-02-21 04:46:12 +0000

    updated makefile

--- util.h
+++ util.h
@@ -1,3 +1,6 @@
+#ifndef UTIL_H
+#define UTIL_H
+
 #include <sys/types.h>
 
 char *binary_to_hex(void *data, ssize_t  n);
@@ -5,3 +8,5 @@ void *hex_to_binary(char *hex, ssize_t *bin_bytes);
 int get_size(char *hex);
 char upper_case(char c);
 int non_hex(char x);
+
+#endif
