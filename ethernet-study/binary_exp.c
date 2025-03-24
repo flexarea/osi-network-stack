ba55c83997fbf72ba76d8103396dc9e43c7a9eac
esdras <esdrasntuyenabo40@gmail.com>
2025-03-24 23:31:27 +0000

    completed template

--- ethernet-study/binary_exp.c
+++ ethernet-study/binary_exp.c
@@ -13,6 +13,8 @@ ssize_t simulation(ssize_t n){
 	unsigned int gen_n;  //generated number	
 	ssize_t table[n][];
 	ssize_t col_dev[];
+	ssize_t completed[n];
+	ssize_t cmp_idx = 0;
 
 	//populate array with slot 0 attempt
 	for(int i=0; i<n; i++){
@@ -21,35 +23,48 @@ ssize_t simulation(ssize_t n){
 
 	//while loop somewhere here
 	while(1){
+		if(cmp_idx == (n-1)){
+			break;
+		}
 		//detect collision
 		ssize_t cdev_len = 0; //number of devices that collided
-		if(col_det(table, col_dev, t, n) == 1) {
-			//generate random number for reach device
-			for(int i=0; i<n; i++){
-				if(in_col_dev(col_dev, cdev_len, i) == 1){
+							  //generate random number for reach device
+		ssize_t pt = ((t-1) < 0) ? 0 : t-1; //previous tslot
+		int col_det = (table, col_dev, pt, n, cdev_len, t); //check for collision & record collided devices
+		for(int i=0; i<n; i++){
+			if(col_det){
+				//check if device collided
+				if(in_col_dev(col_dev, cdev_len, i) == 1){  //if there is collision then the non-collided device doesn't send at this time slot
 					ssize_t rand_n = rand_generator(curr_min, curr_max);
 					ssize_t next_attempt = t + rand_n + 1;
 					table[i][t] = next_attempt;
 				}else{
-					table[i][t] = table[i][t-1];
+					table[i][t] = table[i][pt];
+				}
+
+			}else{
+				//can send frame in this time slot
+				if(table[i][pt] == t){
+					completed[cmp_idx++] = i;	
+				}else{
+					//wait for next attempt
+					table[i][t] = table[i][pt];
 				}
 			}
-
-			
 		}
-
-		
-
-		//the end
 		t++; //move to next timeslot
 	}
+
+
+	return t;
+	//ends here
 }
 ssize_t rand_generator(ssize_t min, ssize_t max){
 	unsigned int buf;	
 
 	if(getrandom(buf, sizeof(buf), 0) == -1){
 		perror("getrandom")
-		return 0;
+			return 0;
 	}
 
 	//scale generated number given range
@@ -66,9 +81,10 @@ int col_det(ssize_t *table_, ssize_t *col_dev_, ssize_t t_, ssize_t n_, ssize_t 
 				continue;
 			}
 			//if device has same slot with 1 other device then append it to list of collided device and move to next
-			if(table_[i][t_] == table[j][t_]){
+			if(table_[i][t_] == table[j][t_] && table[i][t_] == t_){
 				is_collision = 1;
 				col_dev[k] = i;
+				k++;
 				*cdev_len_++;
 				break;
 			}
@@ -84,4 +100,3 @@ int in_col_dev(ssize_t *col_dev_, ssize_t *cdev_len_, ssize_t dev_idx){
 		}
 	}
 }
-
