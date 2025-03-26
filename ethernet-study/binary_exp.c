691caf3ccdc7595406ea8c0ea4a238f61e11b902
esdras <esdrasntuyenabo40@gmail.com>
2025-03-26 19:41:51 +0000

    changed implementation. Using struct

--- ethernet-study/binary_exp.c
+++ ethernet-study/binary_exp.c
@@ -4,8 +4,9 @@
 #include <stdlib.h>
 #include "binary_exp.h"
 
+
 int main(int argc, char *argv[]){
-	ssize_t start_simulation = simulation((ssize_t)argv[0]);
+	ssize_t start_simulation = simulation((ssize_t)argv[1]);
 	if (start_simulation == 0){
 		perror("an error occured");
 	}
@@ -15,94 +16,76 @@ int main(int argc, char *argv[]){
 
 ssize_t simulation(ssize_t n){
 	ssize_t t = 0;
-	ssize_t curr_min = 0;
 	ssize_t curr_max = 1;
-	unsigned int gen_n;  //generated number	
-	ssize_t completed[n];
-	ssize_t cmp_idx = 0;
-	ssize_t capacity = 1;
+	ssize_t number_completed = 0;
+	
+	//populate max range for each devices
+	for(ssize_t i=0; i<n; i++){
+		backoff_range_arr[i] = curr_max;
+	}
 
-
-	ssize_t** table = (ssize_t**) malloc(n*sizeof(ssize_t));
+	struct device_config *devices = (struct device_config*) malloc(n*sizeof(struct device_config));
 	if (table == NULL){
 		perror("table failed to malloc");
 		return 0;
 	}
-	//allocate memory for each rows
-	for(int i=0; i<n; i++){
-		table[i] = (ssize_t*) malloc(capacity*sizeof(ssize_t));
-		if (table[i] == NULL){
-			perror("table failed to malloc");
-			return 0;
-		}
-	}
 
 	//populate array with slot 0 attempt
-	for(int i=0; i<n; i++){
-		table[i][t] = 0;
+	for(ssize_t i=0; i<n; i++){
+		devices[i].max_range = 1;
+		devices[i].next_attempt = 0;
+		devices[i].completed = 0;
 	}
 
 	//while loop somewhere here
 	while(1){
-		if(cmp_idx == (n-1)){
+		if(number_completed == n){
 			break;
 		}
-		ssize_t col_dev[n]; //array of devices that collide (always >= n)
-						   //detect collision
-		ssize_t cdev_len = 0; //number of devices that collided
+		ssize_t devices_w_collision[n]; //array of devices that collide (always >= n)
+		ssize_t number_col_devices = 0; //number of devices that collided
+
+		/*
+		 * TODO: remove if unnecessary
 		ssize_t pt = ((t-1) < 0) ? 0 : t-1; //previous tslot
-		int col_det = (table, col_dev, pt, n, &cdev_len); //check for collision & record collided devices
-		for(int i=0; i<n; i++){
-			if(col_det){
+		*/
+
+		int col_detection = col_det(table, devices_w_collision, t, n, &number_collided_devices); //check for collision & record collided devices
+
+		for(ssize_t i=0; i<n; i++){
+			//check for collison
+			if(col_detection){
+
 				//check if device collided
-				if(in_col_dev(col_dev, cdev_len, i) == 1){  //if there is collision then the non-collided device doesn't send at this time slot
-					ssize_t rand_n = rand_generator(curr_min, curr_max);
-					ssize_t next_attempt = t + rand_n + 1;
-					table[i][t] = next_attempt;
+				//if there is collision then the non-collided device doesn't send at this time slot
 
-				}else{
-					table[i][t] = table[i][pt];
+				if(device_col(col_dev, number_col_devices, i)){
+					ssize_t rand_n = rand_generator(0, table[i].max_range);
+					table[i].next_attempt = t + rand_n + 1; //record next attempt 
+					table[i].max_range = (2**curr_max) - 1;
 				}
-
 
 			}else{
-				//can send frame in this time slot
-				if(table[i][pt] == t){
-					completed[++cmp_idx] = i;	
-				}else{
-					//wait for next attempt
-					table[i][t] = table[i][pt];
-				}
-			}
 
-			//increase buffer size
-			if(t >= capacity){
-				capacity *= 2;
-				ssize_t *ptr = (ssize_t*) realloc(capacity * sizeof(ssize_t))
-				if(ptr == NULL){
-					perror("realloc faild");
-					for(int i=0; i<n; i++){
-						free(table[i]);
-					}
-					free(table);
-					return 0;
+				//No collison. can send frame in this time slot
+				if(table[i].next_attempt == t && !table[i].completed){
+					table[i].completed = 1;	
+					number_completed++;
 				}
-
-				table[i] = ptr;
 			}
 		}
 		t++; //move to next timeslot
 	}
 
-	for(int i=0; i<n; i++){
+	for(ssize_t i=0; i<n; i++){
 		free(table[i]);
 	}
-	free(table);
+
 	return t;
 	//ends here
 }
 ssize_t rand_generator(ssize_t min, ssize_t max){
-	unsigned int buf;	
+	 ssize_t buf;	
 
 	if(getrandom(buf, sizeof(buf), 0) == -1){
 		perror("getrandom")
@@ -110,24 +93,24 @@ ssize_t rand_generator(ssize_t min, ssize_t max){
 	}
 
 	//scale generated number given range
-	return  min + ((ssize_t)buf % (max - min + 1));
+	return  min + (buf % (max - min + 1));
 }
 
 //collision detection
-int col_det(ssize_t *table_, ssize_t *col_dev_, ssize_t t_, ssize_t n_, ssize_t *cdev_len_){
+ssize_t col_det(ssize_t *table_, ssize_t *devices_w_collision_, ssize_t t_, ssize_t n_, ssize_t *number_collision_){
 	ssize_t k=0;
-	int is_collision = 0;
+	ssize_t is_collision = 0;
 	for(ssize_t i=0; i<n_, i++){
 		for(ssize_t j=0; j<n; j++){
 			if(j == i){
 				continue;
 			}
 			//if device has same slot with 1 other device then append it to array of collided device and move to next
-			if(table_[i][t_] == table[j][t_] && table[i][t_] == t_+1){
+			if(table_[i].next_attempt == table[j].next_attempt && table[i].next_attempt == t){
 				is_collision = 1;
-				col_dev[k] = i;
+				devices_w_collision_[k] = i;
 				k++;
-				*cdev_len_++;
+				*number_collision_++;
 				break;
 			}
 		}
@@ -135,9 +118,9 @@ int col_det(ssize_t *table_, ssize_t *col_dev_, ssize_t t_, ssize_t n_, ssize_t 
 	return is_collision;
 }
 //check if device collided
-int in_col_dev(ssize_t *col_dev_, ssize_t *cdev_len_, ssize_t dev_idx){
-	for(int i=0; i<cdev_len; i++){
-		if(col_dev[i] == dev_idx){
+ssize_t device_col(ssize_t *devices_w_collision_, ssize_t *number_collision_, ssize_t device_id){
+	for(ssize_t i=0; i<number_collision_; i++){
+		if(devices_w_collision[i] == device_id){
 			return 1;
 		}
 	}
--- ethernet-study/binary_exp.h
+++ ethernet-study/binary_exp.h
@@ -4,9 +4,15 @@
 #include <sys/types.h>
 #include <stdint.h>
 
+typedef struct device_config{
+	ssize_t max_range;	
+	ssize_t next_attempt;
+	ssize_t completed;
+}device_config;
+
 ssize_t simulation(ssize_t n);
 ssize_t rand_generator(ssize_t min, ssize_t max);
-int col_det(ssize_t *table_, ssize_t *col_dev_, ssize_t t_, ssize_t n_, ssize_t *cdev_len_);
-int in_col_dev(ssize_t *col_dev_, ssize_t *cdev_len_, ssize_t dev_idx);
+int col_det(ssize_t *table_, ssize_t *devices_w_collision, ssize_t t_, ssize_t n_, ssize_t *number_collision_);
+int device_col(ssize_t *devices_w_collision, ssize_t *number_collision_, ssize_t device_id);
 
 #endif
