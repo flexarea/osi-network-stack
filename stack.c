5472f88f496ccbcad7f360f2f11221cb72e4d671
esdras <esdrasntuyenabo40@gmail.com>
2025-04-14 02:30:00 +0000

    fixed small issues

--- stack.c
+++ stack.c
@@ -14,23 +14,6 @@ const int CACHE_LENGTH = 2;
 
 const uint8_t eth_addr[] = {0x12,0x9f,0x41,0x0d,0x0e,0x63}; //Interface ethernet address
 const uint8_t interface_ip[]={0x8c,0xe9,0x01,0x01};       //Interface IP address
-struct table_r routing_table; 
-
-
-struct table_r *table = (struct table_r *) malloc(TABLE_LENGTH*sizeof(struct table_r));
-struct arp_cache *arp_cache_table = (struct arp_cache *) malloc(CACHE_LENGTH*sizeof(struct arp_cache));
-
-/*interface routing table configuration*/
-memcpy(table[0].dest, "\x00\x00\x00\x00",4);
-memcpy(table[0].gateway, "\x0a\x03\x00\x01",4); //send to gateway (next hop)
-memcpy(table[0].genmask, "\x00\x00\x00\x00",4);
-memcpy(table[1].dest, "\x0a\x03\x00\x00",4);
-memcpy(table[1].gateway, "\x00\x00\x00\x00",4); //send to local subnework
-memcpy(table[1].genmask, "\xff\xff\x00\x00",4);
-
-/*ARP cache configuration*/
-memcpy(arp_cache_table[0].ip_addr, "\x0a\x03\x02\x01",4);
-memcpy(arp_cache_table[0].mac_addr, "\x12\x9f\x41\x0d\x0e\x65",4); //send to gateway (next hop)
 
 int eth_cmp(struct frame_fields *frame_f,  const uint8_t *mac_addr);
 
@@ -40,6 +23,21 @@ ssize_t d_size;
 uint32_t cur_cfs; //received cfs
 
 int main(int argc, char *argv[]){
+
+	struct table_r *routing_table = (struct table_r *) malloc(TABLE_LENGTH*sizeof(struct table_r));
+	struct arp_cache *arp_cache_table = (struct arp_cache *) malloc(CACHE_LENGTH*sizeof(struct arp_cache));
+
+	/*interface routing table configuration*/
+	memcpy(table[0].dest, "\x00\x00\x00\x00",4);
+	memcpy(table[0].gateway, "\x0a\x03\x00\x01",4); //send to gateway (next hop)
+	memcpy(table[0].genmask, "\x00\x00\x00\x00",4);
+	memcpy(table[1].dest, "\x0a\x03\x00\x00",4);
+	memcpy(table[1].gateway, "\x00\x00\x00\x00",4); //send to local subnework
+	memcpy(table[1].genmask, "\xff\xff\x00\x00",4);
+
+	/*ARP cache configuration*/
+	memcpy(arp_cache_table[0].ip_addr, "\x0a\x03\x02\x01",4);
+	memcpy(arp_cache_table[0].mac_addr, "\x12\x9f\x41\x0d\x0e\x65",4); //send to gateway (next hop)
 	interface_receiver(&frame_header, &cf_flag, &cur_cfs, &d_size, eth_addr);
 	return 0;
 }
@@ -158,13 +156,13 @@ int eth_cmp(struct frame_fields *frame_f,  const uint8_t *mac_addr){
 }
 
 //function to extract and process ip header fields
-void handle_packet(ssize_t len, struct frame_fields *frame_f, uint8_t *or_frame, struct table_r routing_table_, struct *arp_cache_){
+void handle_packet(ssize_t len, struct frame_fields *frame_f, uint8_t *or_frame, struct arp_cache *arp_cache_){
 	//jump to beginning of ip header
 	uint8_t *fields_ptr = or_frame + 14;
 	//cast to ip struct (extract ip header fields)
 	struct ip_header *packet = (struct ip_header *) fields_ptr;
 	char ip_str[INET_ADDRSTRLEN];
-	struct in_addr ip_addr, genmask_addr;
+	struct in_addr ip_addr;
 	int subnet_list[TABLE_LENGTH];
 	int lg_pfx_idx = -1; //longest subnet prefix index
 	int lg_prefix = -1;
@@ -172,8 +170,8 @@ void handle_packet(ssize_t len, struct frame_fields *frame_f, uint8_t *or_frame,
 
 
 	/* Test print ip addr from packet
-	 printf("IP SRC Address: %s\n", ip_str);	
-	 */
+	   printf("IP SRC Address: %s\n", ip_str);	
+	   */
 
 	/*routing table logic*/	
 
@@ -189,22 +187,22 @@ void handle_packet(ssize_t len, struct frame_fields *frame_f, uint8_t *or_frame,
 	for(int i=0; i<TABLE_LENGTH; i++){
 		char genmask_str[INET_ADDRSTRLEN]; 
 		char dest_str[INET_ADDRSTRLEN];
-		struct in_addr dest_addr, result;
+		struct in_addr dest_addr, genmask_addr, result;
 		int curr_lg_prefix;
 
 		//convert addresses to ip
-		inet_ntop(AF_INET, routing_table_[i].genmask, genmask_str, INET_ADDRSTRLEN);
-		inet_ntop(AF_INET, routing_table_[i].dest, dest_str, INET_ADDRSTRLEN);
+		inet_ntop(AF_INET, routing_table[i].genmask, genmask_str, INET_ADDRSTRLEN);
+		inet_ntop(AF_INET, routing_table[i].dest, dest_str, INET_ADDRSTRLEN);
 
 
 		//convert to bin
 		inet_aton(genmask_str, &genmask_addr);
 		inet_aton(dest_str, &dest_addr);
 		//bitwise-and
-		result.s_addr = ip_addr.s_addr & genmask_addr;
+		result.s_addr = ip_addr.s_addr & genmask_addr.s_addr;
 		//check matching dest addr
-		if(result.s_addr == dest.s_addr){
-			curr_lg_prefix = __builtin_popcount(ntohl(dest.s_addr));
+		if(result.s_addr == dest_addr.s_addr){
+			curr_lg_prefix = __builtin_popcount(ntohl(genmask_addr.s_addr));
 			if(curr_lg_prefix > lg_prefix){
 				lg_prefix = curr_lg_prefix;
 				lg_pfx_idx = i;
@@ -217,7 +215,7 @@ void handle_packet(ssize_t len, struct frame_fields *frame_f, uint8_t *or_frame,
 		//consult arp cache
 		int found_mac = 0;
 		for(int i=0; i<CACHE_LENGTH; i++){
-			if((memcmp(arp_cache_[i].ip_addr, routing_table_[lg_pfx_idx], 4)) == 0){
+			if((memcmp(arp_cache_[i].ip_addr, routing_table[lg_pfx_idx].gateway, 4)) == 0){
 				//cpy arp_cache_[i].mac_addr to frame new dest addr
 				arp_idx = i;
 				found_mac = 1;
@@ -232,17 +230,17 @@ void handle_packet(ssize_t len, struct frame_fields *frame_f, uint8_t *or_frame,
 }
 
 
-ssize_t* encapsulation(struct table_r routing_table_, struct frame_fields *frame_, uint8_t *arp_cache_, int arp_idx_, int lg_pfx_idx_, struct ip_header packet_, ssize_t len){
+void encapsulation(struct frame_fields *frame_, uint8_t *arp_cache_, int arp_idx_, int lg_pfx_idx_, struct ip_header packet_, ssize_t len){
 	/*encapsulation logic*/
-	memcpy(frame_f->src_addr, ether_addr, 6);
-	memcpy(frame_f->dest_addr, arp_cache_[arp_idx_].mac_addr, 6);
+	memcpy(frame_->src_addr, ether_addr, 6);
+	memcpy(frame_->dest_addr, arp_cache_[arp_idx_].mac_addr, 6);
 	memcpy(packet_->src_addr, interface_ip, 4);
-	memcpy(packet_->dest_addr, routing_table_[lg_pfx_idx_].dest, 4);
+	memcpy(packet_->dest_addr, routing_table[lg_pfx_idx_].dest, 4);
 	packet_->ttl--;
 	//recalculate checksum
-	packet->checksum = ip_checksum(packet_);	
+	packet_->checksum = ip_checksum(packet_);	
 	//recreate frame
-	memcpy(frame, packet_+14, len - 14);
+	memcpy(frame_, packet_+14, len - 14);
 }
 
 
