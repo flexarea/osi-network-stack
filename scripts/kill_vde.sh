640bfec2717540c25b89a258b8286981791324aa
esdras <esdrasntuyenabo40@gmail.com>
2025-05-24 02:19:57 +0000

    updated vde kill script

--- scripts/kill_vde.sh
+++ scripts/kill_vde.sh
@@ -3,9 +3,10 @@
 echo "Unplugging switches"
 
 pkill vde_switch
-sudo pkill vde_plug2tap 
+sudo pkill vde_plug2tap
 
 #delete control files
 
-rm -rf /tmp/net1.vde /tmp/net2.vde /tmp/net3.vde
-
+rm -rf /tmp/net*.vde
+rm -rf ~/cs432/cs431/tmp/net*.vde
+rm -rf /home/entuyenabo/cs432/cs431/tmp/net*.vde
